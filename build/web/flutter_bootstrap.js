65206c4c790848d129a7fe2419b2c97beca10680
Mohamud Mohamud <xxhmod.com@gmail.com>
2025-10-28 09:03:26 +0300

    FINAL

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "313034914"
+    serviceWorkerVersion: "1116716584"
   }
 });
